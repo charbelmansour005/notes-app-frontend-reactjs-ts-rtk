4e1dc310fe83544433e24378e1d127cb27fdd93c
charbelmansour005 <charbelmansour005@gmail.com>
2022-11-29 20:08:59 +0200

    random color & style selector

--- src/components/ErrorLinks.tsx
+++ src/components/ErrorLinks.tsx
@@ -1,21 +1,44 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../assets/styles/ErrorLinks.css";
+import { CircularProgress } from "@mui/material";
 
 const ErrorLinks: FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const navigate = useNavigate();
+
+  const handleHomeRedirect = () => {
+    setIsLoading(true);
+    navigate("/");
+  };
+
+  const handleLoginRedirect = () => {
+    setIsLoading(true);
+    navigate("Login");
+  };
+
+  const handleSearchRedirect = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      navigate("Search");
+    }, 500);
+  };
+
   return (
     <>
-      {" "}
-      <a className="link__style" onClick={() => navigate("/")}>
+      <a className="link__style" onClick={handleHomeRedirect}>
         {"←"} Go back Home
       </a>
-      <a className="link__style" onClick={() => navigate("/login")}>
+      <a className="link__style" onClick={handleLoginRedirect}>
         {"←"} Login
       </a>
-      <a className="link__style" onClick={() => navigate("/search")}>
+      <a className="link__style" onClick={handleSearchRedirect}>
         {"←"} Search
       </a>
+      <div className="loader__container">
+        {isLoading && <CircularProgress />}
+      </div>
     </>
   );
 };
--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import "../assets/styles/Page404.css";
 import ErrorLinks from "../components/ErrorLinks";
+import { Paper } from "@mui/material";
 
 const Page404: React.FC = () => {
+  const colors = ["darkgreen", "black", "purple", "darkorange", "gray"];
+  const randomColor = colors[Math.floor(Math.random() * 5)];
+  const color = randomColor;
+  const fontWeights = ["bold", "normal", "lighter"];
+  const randomFontWeight = fontWeights[Math.floor(Math.random() * 3)];
+  const fontWeight = randomFontWeight;
+
   return (
     <div className="centered">
-      <div className="error__container">
-        <p className="title">404 - Page Not Found</p>
+      <Paper className="message__container">
+        <h2 style={{ color: color, fontWeight: fontWeight }}>
+          404 - Page Not Found
+        </h2>
         <p className="body">
           The page you are looking for might have been removed or temporarily
           unavailable.
         </p>
         <ErrorLinks />
-      </div>
+      </Paper>
     </div>
   );
 };
